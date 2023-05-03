e5bb0b43d08ec3f32ff48ed734de116b4d1b3696
Agustina Barbarini <agustinabarbarini02@gmail.com>
2023-05-03 18:41:20 -0300

    PrimerTrabajoPractico

--- Java.js
+++ Java.js
@@ -1,4 +1,19 @@
-function cambiartexto()
-{
-    document.getElementById("Button"). value="Dejar de seguir";
+function cambiartexto(){
+    let element = document.getElementById("Button");
+    if (element.value=="Seguir"){
+        element.value = "Dejar de seguir";
+    } else {
+        element.value = "Seguir";
+    }
 }
+
+function sumarUno(){
+    let contador;
+    if(document.getElementById("contador").value == "200")
+    {
+        let valorVista = parseInt(document.getElementById("contador").value);
+        contador = valorVista + 1;
+    }
+    document.getElementById("contador").value=contador.toString();
+}
+
